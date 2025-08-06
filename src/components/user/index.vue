7ab858da55be8f79f939189e442ef4dd5b89be7d
tadpole <ltadpole@163.com>
2025-08-06 17:18:01 +0800

    feat: user

---
+++ src/components/user/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="user">
+    <div class="user-img">
+      <img :src="getImageUrl('avatar.jpg')" alt="" />
+    </div>
+    <div class="user-motto">给这个世界的留言给这个世界的留言给这个世界的留言</div>
+    <div class="user-statistics">
+      <div class="user-statistics-item">
+        <div class="item-count">19</div>
+        <div class="item-title">文章</div>
+      </div>
+      <el-divider direction="vertical" />
+      <div class="user-statistics-item">
+        <div class="item-count">19</div>
+        <div class="item-title">分类</div>
+      </div>
+    </div>
+    <el-divider />
+    <div class="user-information">
+      <div class="user-information-item">
+        <SvgIcon name="github" width="2.4rem" height="2.4rem" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { getImageUrl } from '@/utils'
+</script>
+
+<style lang="scss" scoped>
+@use 'index' as *;
+</style>
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -13,11 +13,15 @@
         <div class="content-list-item"></div>
       </div>
     </div>
-    <div class="sidebar"></div>
+    <div class="sidebar">
+      <user />
+    </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import user from '@/components/user/index.vue'
+</script>
 
 <style lang="scss" scoped>
 @use 'index.scss' as *;
