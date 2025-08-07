43f1354496d86d16407959dcf2dc8f65ccd91195
tadpole <ltadpole@163.com>
2025-08-07 18:05:27 +0800

    feat: scrollTop

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -11,12 +11,48 @@
       </div>
     </div>
     <layout-footer />
+
+    <SvgIcon
+      name="scrollTop"
+      width="50px"
+      height="50px"
+      class="scroll-top"
+      v-show="isShowScrollTop"
+      @click="scrollToTop"
+    />
   </div>
 </template>
 
 <script setup>
+import { ref, onMounted, onUnmounted } from 'vue'
 import layoutHeader from './header/index.vue'
 import layoutFooter from './footer/index.vue'
+
+const isShowScrollTop = ref(false)
+
+const scrollToTop = () => {
+  document.documentElement.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  })
+}
+
+const handleScroll = () => {
+  const scrollTop = document.documentElement.scrollTop
+  if (scrollTop > 10) {
+    isShowScrollTop.value = true
+  } else {
+    isShowScrollTop.value = false
+  }
+}
+
+onMounted(() => {
+  window.addEventListener('scroll', handleScroll)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('scroll', handleScroll)
+})
 </script>
 
 <style lang="scss" scoped>
